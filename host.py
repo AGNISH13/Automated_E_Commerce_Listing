486060c04529261105bbaf3794250c618a4248d4
BRohit31 <rohit.ronti@gmail.com>
2025-04-10 13:21:55 +0530

    added pipeline to handle multiple file uploads at once

--- host.py
+++ host.py
@@ -3,65 +3,48 @@ from datetime import datetime
 import os
 import streamlit as st
 
+from pipeline import pipeline
 from crud import video, transcripts, keyframes, keyframe_classes, final_class
 from poc_whisper.whisper import transcribe_audio_from_bytes  # Importing Whisper processing
-# from poc_katna.katna import extract_keyframes    # Importing Katna processing
-# from poc_filtering.filtering import filter_images  # Importing Filtering
-# from poc_yolo.yolo2 import detect_objects  # Importing YOLO detection
 
-# other functions
-def get_video_path():
-    return file_path
-
-def create_video_obj():
-    videoobj = video()
-    video_id = videoobj.create(video_link=st.session_state['file_path'])
-    return None
-
-def whisper():
-    transcript = transcribe_audio_from_bytes(st.session_state['file_path']) #  return audio transcript -> string
-    transcriptobj = transcripts()
-    transcipt_id = transcriptobj.create(video_id=st.session_state['video_id'],transcript=transcript)
-    return transcript
-
-# VIDEO UPLOAD PAGE ---------------------------------------------
-# Choose whether to upload file or input file path
-# Create a duplicate of the file in the uploads folder
-# If the file already exists, remove it and create a new one
+'''
+VIDEO UPLOAD PAGE ---------------------------------------------
+Choose whether to upload file or input file path
+Create a duplicate of the file in the uploads folder
+If the file already exists, remove it and create a new one
+'''
 
 # st.set_page_config(page_title="Video Upload", page_icon="📹", layout="wide")
-st.write("""## Uploading video""") 
+# st.write("""## Uploading video""") 
 
-session_start_time = str(datetime.now())
-if 'session_start_time' not in st.session_state:
-    st.session_state['session_start_time'] = session_start_time
-
-t = st.session_state['session_start_time'].replace(" ", "_").replace(":", "-").replace(".", "-")
-file_path = "stlit/uploads/video_at_"+t+".mp4"
-
-if 'file_path' not in st.session_state:
-    st.session_state['file_path'] = file_path
-
-if os.path.isfile(file_path):
-    os.remove(file_path)
-
-uploaded_file = st.file_uploader("Upload video", type=["mp4"])
+uploaded_file = st.file_uploader("choose videos", type=["mp4"], accept_multiple_files=True, label_visibility="hidden")
 if uploaded_file:
-    video_display = st.video(uploaded_file, format="video/mp4")
-    with open(file_path, "wb") as write_file:
-        write_file.write(uploaded_file.getvalue())
+    session_start_time = str(datetime.now())
+    if 'session_start_time' not in st.session_state:
+        st.session_state['session_start_time'] = session_start_time.replace(" ", "_").replace(":", "-").replace(".", "-")
+
+    for file in uploaded_file:
+        # video_display = st.video(uploaded_file, format="video/mp4")
+        name = file.name
+        file_path = "stlit/uploads/"+name+"upload_at_"+st.session_state['session_start_time']+".mp4"
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+        with open(file_path, "wb") as write_file:
+            write_file.write(uploaded_file.getvalue())
 
     # RUN POC STEPS ---------------------------------------------
 
-    # create video id and send to database
-    if 'video_id' not in st.session_state:
-        st.session_state['video_id'] = create_video_obj()
-
-    # run whisper and display transcript
-    if 'transcript' not in st.session_state:
-        st.session_state['transcript'] = whisper()
+    video_path_list = []
+    for file in os.path("stlit/uploads/"):
+        if file.endswith(st.session_state["session_start_time"]+".mp4"):
+            video_path_list.append(file)
     
-    if os.path.isfile(file_path):
-        st.write("""## Transcript""") 
-        st.write("Transcribing audio with Whisper...")
-        st.write(st.session_state['transcript'])
+    # create video id and send to database
+    steps = pipeline(video_path_list)
+    video_id_list = steps.create_video_obj()
+
+    if 'video_id' not in st.session_state:
+        st.session_state['video_id'] = video_id_list # update session state with list of video ids
+
+    transcript_list = steps(file_path).run_pipeline()
+
---
+++ pipeline.py
@@ -0,0 +1,114 @@
+from crud import video, transcripts, keyframes, keyframe_classes,final_class
+from host import get_video_path
+from poc_whisper.whisper import transcribe_audio_from_bytes  # Importing Whisper processing
+from poc_katna.katna import extract_keyframes    # Importing Katna processing
+from poc_filtering.filtering import filter_images  # Importing Filtering
+from poc_yolo.yolo2 import detect_objects  # Importing YOLO detection
+from poc_gemini.top_ids import identify_product  # Importing Gemini classification
+from poc_blip.caption_matching_blip import blip_filter  # Importing BLIP captioning
+from poc_gemini.description import generate_product_description  # Importing Gemini classification
+
+
+class pipeline:
+    def __init__(self, video_paths):
+        self.video_paths = video_paths
+        self.video_ids = []
+
+    def create_video_obj(self):
+        video_id = []
+        for file in self.video_paths:
+            videoobj = video()
+            video_id.append(videoobj.create(video_link=self.video_path))
+        self.video_ids = video_id
+        return video_id
+
+    def run_pipeline(self): # update with stepper
+        transcripts = []
+        for i, path in enumerate(self.video_paths):
+            # Process audio with Whisper
+            print("Processing audio with Whisper...")
+            transcript = transcribe_audio_from_bytes(self.video_path)  # return audio transcript -> string
+            transcripts.append(transcript)
+
+            # Create a transcript object and store the transcript
+            transcriptobj = transcripts()
+            transcipt_id = transcriptobj.create(video_id=self.video_ids[i], transcript=transcript)
+            print(transcipt_id)
+            print(transcriptobj.get_transcripts(video_id=self.video_ids[i])['response'])
+
+            return transcripts
+
+# def main():
+
+#     video_path = get_video_path()
+    
+#     videoobj = video()
+#     video_id = videoobj.create(video_link=video_path)
+#     print(video_id)
+    
+#     print("Processing audio with Whisper...")
+#     transcript = transcribe_audio_from_bytes(video_path) #  return audio transcript -> string
+    
+#     transcriptobj = transcripts()
+#     transcipt_id = transcriptobj.create(video_id=video_id,transcript=transcript)
+#     print(transcipt_id)
+#     print(transcriptobj.get_transcripts(video_id=video_id)['response'])
+
+#     print("Extracting keyframes using Katna...")
+#     keyframe_bins = extract_keyframes(video_path)
+#     keyframeobj = keyframes()
+#     keyframes_id = keyframeobj.create(video_id=video_id,keyframes=keyframe_bins)
+#     print(keyframes_id)
+    
+#     keyframe_r=[] # keyframeobj.get_all_keyframes(video_id=video_id)['response'] !FIX
+#     print("Applying profanity filter...")
+#     for keyframe_collection in keyframeobj.get_all_keyframes(video_id=video_id)['response']:
+#         keyframe_r.append(keyframe_collection)
+#     # print(keyframe_r)
+#     filtered_keyframes = filter_images(keyframe_r)
+#     keyframeobj.update_keyframes(video_id, filtered_keyframes)
+#     # print(filtered_keyframes)# return filtered_keyframes -> list[base64] 
+    
+#     # store in database
+
+#     print("Running YOLO object detection...")
+#     # detected_keyframes, class_ids = detect_objects(filtered_keyframes)
+#     class_ids = detect_objects(video_id,filtered_keyframes)
+#     classobj = keyframe_classes()
+#     for classid in class_ids:
+#         classobj.create(video_id=classid['video_id'],keyframe_id=classid['keyframe_id'],classes=classid['class'])
+#     print(class_ids[-1])
+    
+#     # Selecting top product using Gemini
+#     print("Identifying the product with Gemini...")
+#     classobj = keyframe_classes()
+#     class_ids = classobj.get_classes(video_id=video_id)['response']
+#     confirmed_ids = identify_product(transcript,class_ids)  # Example user input
+#     finalclassobj = final_class()
+#     finalclassobj.create(video_id,classes=confirmed_ids)
+#     print(f"Confirmed Product IDs: {confirmed_ids}")
+#     # confirmed_ids = ["bottle"]
+
+#     # Selecting final keyframes using BLIP
+#     print("Selecting final keyframes with BLIP...")
+    
+#     blip_frame_obj = keyframes()
+#     finalclassobj = final_class()
+#     selected_keyframes = blip_frame_obj.get_all_keyframes(video_id)["response"]
+#     confirmed_ids = finalclassobj.get_final_classes(video_id=video_id)['response']
+#     matched_keyframes = blip_filter(selected_keyframes, confirmed_ids)
+
+#     print(f"No. of keyframes selected by BLIP:\n{len(matched_keyframes)}")
+    
+#     # Generate the product description
+#     print("Generating product description using Gemini...")
+#     transcriptobj = transcripts()
+#     transcript = transcriptobj.get_transcripts(video_id=video_id)['response']
+#     product_description = generate_product_description(transcript)
+
+#     print(f"Product Description:\n{product_description}")
+
+
+
+# if __name__ == "__main__":
+#     main()
